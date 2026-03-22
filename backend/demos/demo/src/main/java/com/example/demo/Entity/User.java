9a85c7671cbad527f7b3ff9eb0856075a3d2921e
Amlesh <harsh6387842@gmail.com>
2026-03-23 03:32:17 +0530

    Add getter and setter methods for User entity

--- backend/demos/demo/src/main/java/com/example/demo/Entity/User.java
+++ backend/demos/demo/src/main/java/com/example/demo/Entity/User.java
@@ -38,4 +38,60 @@ public class User {
 
     @NotNull
     private Boolean verified = false;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Integer getOtp() {
+        return otp;
+    }
+
+    public void setOtp(Integer otp) {
+        this.otp = otp;
+    }
+
+    public Boolean getVerified() {
+        return verified;
+    }
+
+    public void setVerified(Boolean verified) {
+        this.verified = verified;
+    }
 }
